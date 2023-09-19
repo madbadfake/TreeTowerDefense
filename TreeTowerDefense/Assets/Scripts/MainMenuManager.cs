06b3f7d58afe531d2f01cb4540b96075ceef0504
Celine <celine-heublein@web.de>
2023-09-19 23:06:27 +0200

    - Added Options Menu and fixed Main Menu

---
+++ TreeTowerDefense/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button optionButton;
+    public Button exitButton;
+
+    public GameObject optionCanvas;
+    public GameObject menuCanvas;
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        optionButton.onClick.AddListener(Options);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("EditScene");
+    }
+
+    public void Options()
+    {
+        menuCanvas.SetActive(false);
+        optionCanvas.SetActive(true);
+    }
+
+    public void Back()
+    {
+        menuCanvas.SetActive(true);
+        optionCanvas.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
